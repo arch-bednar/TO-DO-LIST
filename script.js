4615525cb7bffaf9f2f92ad1f54fd4bdd8793f06
Dawid Bednarczyk <dawid.bednarczyk@protonmail.com>
2024-08-04 10:48:07 -0400

    second commit

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 var list = new Array();
+var number = 0;
 
 function addToList(){
-    list.push(document.getElementsByName("userInput").value);
+
+    let element = document.getElementById("userInput");
+
+    if (element.value == null || element.value == undefined || element.value.length == 0){
+        return;
+    }
+
+    list.push(element.value);
+    let x = document.getElementById("todo-list");
+    //x.innerHTML += `<il id="${number}">${element.value}</il>`;
+    x.appendChild(document.createElement("li"));
+    number += 1;
+    element.value = "";
+    //element.reset();
 }
 
 function removeFromList(){
@@ -9,5 +23,8 @@ function removeFromList(){
 }
 
 function addEvent(){
+
+}
+function removeFromList(){
 
 }
